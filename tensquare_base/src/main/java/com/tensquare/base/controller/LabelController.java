6ea96770ad52dbac4d9c94d710d8bb48ecbe0f3b
aslhdhalshd <1341714009@qq.com>
2019-11-21 17:20:58 +0800

    update

--- tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -21,42 +21,42 @@ public class LabelController {
 
     @RequestMapping(method = RequestMethod.GET)
     public Result findAll(){
-        return new Result(true, StatusCode.Ok,"查询成功",labelService.findAll());
+        return new Result(true, StatusCode.OK,"查询成功",labelService.findAll());
     }
 
     @RequestMapping(value = "/{labelId}",method = RequestMethod.GET)
     public Result findById(@PathVariable String labelId){
-        return new Result(true, StatusCode.Ok,"查询成功",labelService.findById(labelId));
+        return new Result(true, StatusCode.OK,"查询成功",labelService.findById(labelId));
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public Result save(@RequestBody Label label){
         labelService.save(label);
-        return new Result(true, StatusCode.Ok,"添加成功");
+        return new Result(true, StatusCode.OK,"添加成功");
     }
 
     @RequestMapping(value = "/{labelId}",method = RequestMethod.PUT)
     public Result update(@PathVariable String labelId, @RequestBody Label label){
         label.setId(labelId);
         labelService.update(label);
-        return new Result(true, StatusCode.Ok,"修改成功");
+        return new Result(true, StatusCode.OK,"修改成功");
     }
 
     @RequestMapping(value = "/{labelId}",method = RequestMethod.DELETE)
     public Result deleteById(@PathVariable String labelId){
         labelService.deleteById(labelId);
-        return new Result(true, StatusCode.Ok,"删除成功");
+        return new Result(true, StatusCode.OK,"删除成功");
     }
 
     @RequestMapping(value = "/search",method = RequestMethod.POST)
     public Result findSearth(@RequestBody Label label){
         List<Label> list=labelService.findSearch(label);
-        return new Result(true, StatusCode.Ok,"查询成功",list);
+        return new Result(true, StatusCode.OK,"查询成功",list);
     }
 
     @RequestMapping(value = "/search/{page}/{size}",method = RequestMethod.POST)
     public Result pageQuery(@RequestBody Label label, @PathVariable int page, @PathVariable int size){
         Page<Label> pageData=labelService.pageQuery(label,page,size);
-        return new Result(true, StatusCode.Ok,"查询成功",new PageResult<Label>(pageData.getTotalElements(),pageData.getContent()));
+        return new Result(true, StatusCode.OK,"查询成功",new PageResult<Label>(pageData.getTotalElements(),pageData.getContent()));
     }
 }
